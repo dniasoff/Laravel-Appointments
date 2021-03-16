a0e0cdc306eef60625362bc3563e6afdfcc9705e
Daniel Niasoff <4070285+dniasoff@users.noreply.github.com>
2021-03-16 21:53:07 +0000

    fixes

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from '@aws-cdk/core';
-import * as ecr from "@aws-cdk/aws-ecr";
+import * as cfninc from '@aws-cdk/cloudformation-include';
 import * as ecs from "@aws-cdk/aws-ecs";
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: cdk.Construct, id: string, branch: string, serviceName: string) {
+    super(scope, id);
 
-     // ECS Cluster
-     const cluster = ecs.Cluster.fromClusterAttributes(this, "Cluster", {
 
-     })
+    const template = new cfninc.CfnInclude(this, 'Template', { 
+      templateFile: 'stack.yml',
+    });
+      
+    const laravelTask =  template.getResource("laravelTaskDef9B0924CC") as ecs.CfnTaskDefinition;
 
+    const laravelTaskDefintionsArray =  laravelTask.containerDefinitions as Array<ecs.CfnTaskDefinition.ContainerDefinitionProperty>;
+
+
+    let laravelTaskDefintion = laravelTaskDefintionsArray[1];
+
+    
 
   }
   
